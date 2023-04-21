065cbbf151731da72ef62aa014e0dbf2e31624e3
chief <zhaofei@topcj.com>
2023-04-22 00:25:58 +0800

    feat: 修改title

--- app/components/home.tsx
+++ app/components/home.tsx
@@ -167,9 +167,9 @@ function _Home() {
         className={styles.sidebar + ` ${showSideBar && styles["sidebar-show"]}`}
       >
         <div className={styles["sidebar-header"]}>
-          <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+          <div className={styles["sidebar-title"]}>水滴</div>
           <div className={styles["sidebar-sub-title"]}>
-            Build your own AI assistant.
+            更快的AI助手.
           </div>
           <div className={styles["sidebar-logo"]}>
             <ChatGptIcon />
